5406e35b9fd7c6ed9ea1f4faadcea2bbbf531c12
Bogdan <sandubogdan2001@gmail.com>
2023-12-20 15:54:15 +0200

    security

--- src/role/role.guard.ts
+++ src/role/role.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Role } from '../types/Role';
+import { Role } from '../types/role';
+
 import { ROLES_KEY } from '../constants/role';
 
 @Injectable()
